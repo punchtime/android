61ebabddc47e4bbf3c8364876ee77fab650c3307
Arnaud Weyts <arnaud.weyts@student.odisee.be>
2016-04-22 18:16:00 +0200

    fixed crash when scrolling vertically, changed viewpager to custom one without swiping

---
+++ app/src/main/java/io/punchtime/punchtime/ui/custom/CustomViewPager.java
@@ -0,0 +1,32 @@
+package io.punchtime.punchtime.ui.custom;
+
+/**
+ * Created by Arnaud on 4/22/2016.
+ */
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
