69930b59929efeb180f3fcb02cb17c47bad38ca7
Elias Meire <eliasmeire.dbz@gmail.com>
2016-05-06 05:38:55 +0200

    Counter

---
+++ app/src/main/java/io/punchtime/punchtime/logic/loaders/TimerLoader.java
@@ -0,0 +1,49 @@
+package io.punchtime.punchtime.logic.loaders;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.Loader;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by elias on 28/04/16.
+ * for project: Punchtime
+ */
+public class TimerLoader extends Loader<Void> {
+    BroadcastReceiver mBroadcastReceiver;
+
+    public TimerLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    deliverResult(null);
+            }
+        };
+
+        getContext().registerReceiver(mBroadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+    @Override
+    protected void onStopLoading() {
+        if (mBroadcastReceiver != null)
+            getContext().unregisterReceiver(mBroadcastReceiver);
+    }
+    @Override
+    protected void onForceLoad() {
+        deliverResult(null);
+    }
+
+    @Override
+    protected void onReset() {
+    }
+}
