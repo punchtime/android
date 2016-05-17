f2e1187844ab06d29d17b4646a97dc3bec257296
Haroen Viaene <hello@haroen.me>
2016-05-17 11:22:09 +0200

    remove needless import

--- app/src/main/java/io/punchtime/punchtime/ui/fragments/SettingsFragment.java
+++ app/src/main/java/io/punchtime/punchtime/ui/fragments/SettingsFragment.java
@@ -5,13 +5,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
