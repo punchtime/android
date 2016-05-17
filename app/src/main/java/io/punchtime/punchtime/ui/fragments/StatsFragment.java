f59a95e40feb758df9568387c1bbad4d347a82a0
Elias Meire <eliasmeire.dbz@gmail.com>
2016-05-17 15:39:22 +0200

    Working statistics

--- app/src/main/java/io/punchtime/punchtime/ui/fragments/StatsFragment.java
+++ app/src/main/java/io/punchtime/punchtime/ui/fragments/StatsFragment.java
@@ -1,17 +1,27 @@
 package io.punchtime.punchtime.ui.fragments;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.EdgeDetail;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
 import com.hookedonplay.decoviewlib.events.DecoEvent;
 
+import java.util.ArrayList;
+
 import io.punchtime.punchtime.R;
+import io.punchtime.punchtime.data.Pulse;
 import io.punchtime.punchtime.ui.activities.MainActivity;
 
 /**
@@ -21,6 +31,11 @@ public class StatsFragment extends Fragment {
     private  MainActivity activity;
     private DecoView weekArcView;
     private DecoView dayArcView;
+    private TextView dayHoursWorked;
+    private TextView weekHoursWorked;
+
+    private int seriesDayIndex;
+    private int seriesWeekIndex;
 
     public StatsFragment() {
         Bundle args = new Bundle();
@@ -38,22 +53,46 @@ public class StatsFragment extends Fragment {
         // Setup toolbar
         activity.setTitle(R.string.menu_stats);
 
-        // Store charts
+        // Store UI
         weekArcView = (DecoView) v.findViewById(R.id.weekArcView);
         dayArcView = (DecoView) v.findViewById(R.id.dayArcView);
+        dayHoursWorked = (TextView) v.findViewById(R.id.dayHoursWorked);
+        weekHoursWorked = (TextView) v.findViewById(R.id.weekHoursWorked);
 
         // Setup charts
         setupWeekArc();
         setupDayArc();
 
+        // get Firebase data
+        getStatistics();
+
         return v;
     }
 
+    private void getStatistics() {
+        if (activity.getAuth() != null) {
+            Query query = activity.getFirebaseRef().child("users").child(activity.getAuth().getUid()).child("pulses").limitToLast(200);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    ArrayList<Pulse> pulses = new ArrayList<>();
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        pulses.add(child.getValue(Pulse.class));
+                    }
+                    new CalculateHoursWorked().execute(pulses);
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+        }
+    }
+
     private void setupDayArc() {
-        // TODO: 17/05/16 fill in arcview with data from firebase (month and day) 
-        // TODO: 17/05/16 where in firebase is this stored and how is it changed
         // Create background track
-        dayArcView.addSeries(new SeriesItem.Builder(Color.argb(56,0,0,0))
+        dayArcView.addSeries(new SeriesItem.Builder(Color.argb(100,0,0,0))
                 .setRange(0, 100, 100)
                 .setLineWidth(32f)
                 .build());
@@ -64,7 +103,7 @@ public class StatsFragment extends Fragment {
                 .setLineWidth(32f)
                 .build();
 
-        dayArcView.addSeries(seriesItem2);
+        seriesDayIndex = dayArcView.addSeries(seriesItem2);
 
         dayArcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
                 .setDelay(0)
@@ -74,18 +113,19 @@ public class StatsFragment extends Fragment {
 
     private void setupWeekArc() {
         // Create background track
-        weekArcView.addSeries(new SeriesItem.Builder(Color.argb(56,0,0,0))
+        weekArcView.addSeries(new SeriesItem.Builder(Color.argb(100,0,0,0))
                 .setRange(0, 100, 100)
                 .setLineWidth(32f)
                 .build());
 
         //Create data series track
-        SeriesItem seriesItem1 = new SeriesItem.Builder(getResources().getColor(R.color.colorAccent))
+        SeriesItem seriesItemWeek = new SeriesItem.Builder(getResources().getColor(R.color.colorAccent))
                 .setRange(0, 100, 0)
+                .setInitialVisibility(false)
                 .setLineWidth(32f)
                 .build();
 
-        weekArcView.addSeries(seriesItem1);
+        seriesWeekIndex = weekArcView.addSeries(seriesItemWeek);
 
         weekArcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
                 .setDelay(0)
@@ -106,5 +146,42 @@ public class StatsFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    private void updateChartUI(double hoursDay, double hoursWeek) {
+        // TODO: 17/05/16 Don't hardcode values
+        int quotaDay = 8;
+        int quotaWeek = 38;
+        dayArcView.addEvent(new DecoEvent.Builder((int)((hoursDay / quotaDay) * 100)).setIndex(seriesWeekIndex).setDuration(2000).build());
+        weekArcView.addEvent(new DecoEvent.Builder((int)((hoursWeek / quotaWeek) * 100)).setIndex(seriesDayIndex).setDuration(2000).build());
+
+        dayHoursWorked.setText((Math.round(hoursDay * 10.0) / 10.0) + " / " + quotaDay);
+        weekHoursWorked.setText((Math.round(hoursWeek * 10.0) / 10.0) + " / " + quotaWeek);
+    }
+
+    private class CalculateHoursWorked extends AsyncTask<ArrayList<Pulse>, double[], double[]> {
+        @Override
+        protected double[] doInBackground(ArrayList<Pulse>... params) {
+            ArrayList<Pulse> pulses = params[0];
+            double hoursWorkedToday = 0, hoursWorkedWeek = 0, hoursWorkedPulse;
+            for (Pulse pulse: pulses) {
+                if(pulse.getCheckin() < (System.currentTimeMillis() - 6.048e+8)) continue;
+
+                hoursWorkedPulse = pulse.getCheckout() != 0 ? (pulse.getCheckout() - pulse.getCheckin()) / 3.6e+6 : (System.currentTimeMillis() - pulse.getCheckin()) / 3.6e+6;
+
+                if(pulse.getCheckin() > (System.currentTimeMillis() - 6.048e+8)) {
+                    hoursWorkedWeek += hoursWorkedPulse;
+                    if(pulse.getCheckin() > (System.currentTimeMillis() - 8.64e+7)) hoursWorkedToday += hoursWorkedPulse;
+                }
+            }
+
+            return new double[] {hoursWorkedToday, hoursWorkedWeek};
+        }
+
+        @Override
+        protected void onPostExecute(double[] hoursWorked) {
+            super.onPostExecute(hoursWorked);
+            updateChartUI(hoursWorked[0], hoursWorked[1]);
+        }
     }
 }
