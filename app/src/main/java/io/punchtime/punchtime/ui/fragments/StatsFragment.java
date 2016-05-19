3f678d9fd25154e0e6d9e60f21c42e137de652d0
Elias Meire <eliasmeire.dbz@gmail.com>
2016-05-19 17:26:10 +0200

    unused import

--- app/src/main/java/io/punchtime/punchtime/ui/fragments/StatsFragment.java
+++ app/src/main/java/io/punchtime/punchtime/ui/fragments/StatsFragment.java
@@ -5,7 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
