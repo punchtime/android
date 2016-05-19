7514608bf7c9209b7f1ff9dc17d7ff23fcae7921
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-05-19 22:43:45 +0200

    calender now only shows pulses for company you're in

--- app/src/main/java/io/punchtime/punchtime/logic/operations/PulseOperations.java
+++ app/src/main/java/io/punchtime/punchtime/logic/operations/PulseOperations.java
@@ -1,30 +1,36 @@
 package io.punchtime.punchtime.logic.operations;
 
 //import android.os.AsyncTask;
+
 import android.support.v4.util.LongSparseArray;
+import android.support.v7.preference.PreferenceManager;
 
 import com.alamkanak.weekview.WeekView;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-//import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import io.punchtime.punchtime.data.Pulse;
+import io.punchtime.punchtime.ui.activities.MainActivity;
+
+//import com.firebase.client.Query;
 
 /**
  * Created by arnaud on 15/05/16.
  */
 public class PulseOperations {
     private Firebase mRef;
+    private MainActivity activity;
     private LongSparseArray<Pulse> pulseArray;
     private LongSparseArray<String> keyArray;
 
-    public PulseOperations(Firebase mRef) {
+    public PulseOperations(Firebase mRef, MainActivity activity) {
         this.mRef = mRef;
+        this.activity = activity;
         pulseArray = new LongSparseArray<>();
         keyArray = new LongSparseArray<>();
     }
@@ -56,9 +62,12 @@ public class PulseOperations {
                 pulseArray.clear();
                 long key = 0;
                 for(DataSnapshot child : dataSnapshot.getChildren()) {
-                    pulseArray.put(key, child.getValue(Pulse.class));
-                    keyArray.put(key, child.getKey());
-                    key++;
+                    String currentCompany = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext()).getString("pref_current_company","");
+                    if(currentCompany.equals(child.child("employer").getValue(String.class))) {
+                        pulseArray.put(key, child.getValue(Pulse.class));
+                        keyArray.put(key, child.getKey());
+                        key++;
+                    }
                 }
 
                 // Here we know pulsesList is filled
